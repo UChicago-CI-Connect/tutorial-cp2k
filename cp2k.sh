5841cc6d382bd8e0f6902809ecb192978ecb9c50
bala <dmbala@gmail.com>
2014-11-24 15:11:48 -0600

    revert grid universe

--- cp2k.sh
+++ cp2k.sh
@@ -2,3 +2,6 @@
   source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
   module load cp2k
   cp2k.x $1 > cs_cell_opt_direct_lbfgs.out
+  mkdir -p OutPutDir
+  cp * OutPutDir/.
+  tar cvzf OutPutDir.tar.gz OutPutDir
