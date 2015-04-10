a0ad182fb21cd620aa6f2002f7020cfe5c4bb59d
Suchandra Thapa <sthapa@ci.uchicago.edu>
2015-04-10 16:28:37 -0500

    Don't need to source lmod file anymore

--- cp2k.sh
+++ cp2k.sh
@@ -1,7 +1,6 @@
 #!/bin/bash
-  source /cvmfs/oasis.opensciencegrid.org/osg/modules/lmod/5.6.2/init/bash
-  module load cp2k
-  cp2k.x $1 > cs_cell_opt_direct_lbfgs.out
-  mkdir -p OutPutDir
-  cp * OutPutDir/.
-  tar cvzf OutPutDir.tar.gz OutPutDir
+module load cp2k
+cp2k.x $1 > cs_cell_opt_direct_lbfgs.out
+mkdir -p OutPutDir
+cp * OutPutDir/.
+tar cvzf OutPutDir.tar.gz OutPutDir
